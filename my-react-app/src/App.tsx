ae0761dc01a998253c886bd1d6a062ed82c78a3f
vishal-git9 <vishalsinghrajawat990@gmail.com>
2023-05-23 01:00:52 +0530

    responsive for both medium and small screens

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,11 +3,11 @@ import Sidebar from "./components/sidebar";
 import { Allroutes } from "./routes/Allroutes";
 function App() {
     return (
-    <div className="flex flex-row gap-10 w-9/10 ">
-      <div className="basis-1/4">
+    <div className="flex lg:flex-row sm:flex-col gap-10 w-9/10 ">
+      <div className="lg:basis-1/4 sm:4/4">
     <Sidebar/>
     </div>
-    <div className="basis-3/4">
+    <div className="basis-3/4 sm:4/4">
     <Allroutes/>
     </div>
     </div>
--- my-react-app/src/components/sidebar.tsx
+++ my-react-app/src/components/sidebar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Sidebar() {
   return (
     <div className="flex">
-      <div className="flex flex-col h-screen p-3 bg-white shadow w-60">
+      <div className="flex flex-col lg:h-screen sm:h-fit p-3 bg-white shadow lg:w-60 sm:w-full bg-black">
         <div className="space-y-3">
           <div className="flex items-center">
             <h2 className="text-xl font-bold">Home</h2>
